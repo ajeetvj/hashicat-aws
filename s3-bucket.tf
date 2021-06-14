e3d8f974e3d91ed52886b00a9558afe4c99a384f
Ajeet <avjoshi.1989@gmail.com>
2021-06-14 12:34:40 +0000

    Fixed 2 s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,5 @@
 module "s3_bucket" {
   source  = "app.terraform.io/ajeet-training/s3-bucket/aws"
   version = "2.2.0"
-
-  bucket = "terraform-chip-training-avj"
-  acl    = "private"
   bucket_prefix = "ajeet"
 }
